da9d824b15c375bfc199b1e424d8c22789dd7d8f
FVargas4 <fvargascode@gmail.com>
2021-04-03 18:47:46 -0600

    Agregar NavBar

--- VGTrade/resources/views/pagina-inicial.blade.php
+++ VGTrade/resources/views/pagina-inicial.blade.php
@@ -12,7 +12,33 @@
       </head>
       <body class="bg-dark">
         <br>
-        <h1 class="text-center text-light">Integrantes del equipo</h1>
+        <h1 class="text-center text-light">Equipo 5 - LDAW</h1>
+        <h2 class="text-center text-light">Integrantes del equipo</h2>
+        <br>
+        <nav class="navbar navbar-expand-lg navbar-light bg-white">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{url('pi')}}">Integrantes del equipo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                    </ul>
+                    <span class="navbar-text">
+                        Navbar text with an inline element
+                    </span>
+                </div>
+            </div>
+        </nav>
         <br>
         <div class="row">
             <div class="col-sm-6">
